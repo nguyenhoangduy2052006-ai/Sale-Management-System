fabc22a63561d75e719c803b9ac32ebc2d0b956e
Nguyen Hoang Duy <nguyenhoangduy20.5.2006@gmail.com>
2026-06-10 23:18:51 +0700

    Update code for ProductManagement

--- src/Management/ProductManagement.java
+++ src/Management/ProductManagement.java
@@ -3,36 +3,65 @@ package Management;
 
 import Entity.Product;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ProductManagement {
-    private ArrayList<Product> ProductList; // Khai bao bien
+    private ArrayList<Product> productList; // Khai bao bien
     
     public ProductManagement () {
-        this.ProductList= new ArrayList<>(); 
+        this.productList= new ArrayList<>(); 
     }
     
     // findProductById Function 
-    public Product findProductById () {
-        
+    public Product findProductById (String productId) {
+        for (Product p : productList) {
+            if (p.getProductId().equalsIgnoreCase(productId)) {
+                return p;
+            } 
+        }
+        return null;
     }
     
     // addNewProduct fuction
-    public Product addNewProdtuct () {
-        
+    public boolean addNewProdtuct (Product newProduct) {
+       if (findProductById(newProduct.getProductId())!=null) {
+           System.out.println("Error. Productt Id was existed: " + newProduct.getProductId());
+           return false;
+       } else {
+           this.productList.add(newProduct);
+           return true;
+       }
     }
     
     // updateProductInfo
-    public Product updateProductInfo () {
-        
+    public boolean updateProductInfo (String productId, String newName, double newPrice)  {
+        Product p= findProductById (productId);
+        if (p==null) {
+            System.out.println("Error. Not found product to update.");
+            return false;
+        } else {
+            p.setProductName(newName);
+            p.setPrice(newPrice);
+            return true;
+        }
     }
     
     //removeProduct
-    public Product removeProduct () {
-        
+    public boolean removeProduct (String productId) {
+        Product p=findProductById(productId);
+        if (p==null) {
+            System.out.println("Error. Not found product to delete: " + productId);
+            return false;
+        } else {
+            this.productList.remove(p);
+            return true;
+        }
     }
     // displayProductList function
-    public Product displayProductList () {
-        
+    public void displayProductList () {
+        for (Product p : this.productList) {
+            System.out.println(p.toString());
+        }
     }
     
 }
