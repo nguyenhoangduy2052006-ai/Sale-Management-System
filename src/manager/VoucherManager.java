b161ea1b3b1891d5fe9384a9d2cccd4f6263f6ce
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-06-15 19:22:15 +0700

    Update VoucherManager.java

--- src/manager/VoucherManager.java
+++ src/manager/VoucherManager.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package manager;
 
-/**
- *
- * @author Nguyen Hoang Duy
- */
-public class VoucherManager {
-    
+import model.voucher.Voucher;
+import java.util.ArrayList;
+
+public class VoucherManagement {
+
+    // Encapsulation
+    private ArrayList<Voucher> voucherList;
+
+    // Constructor
+    public VoucherManagement() {
+        voucherList = new ArrayList<>();
+    }
+
+    // Thêm voucher
+    public void addVoucher(Voucher voucher) {
+        voucherList.add(voucher);
+    }
+
+    // Kiểm tra voucher tồn tại
+    public boolean checkVoucher(String voucherCode) {
+
+        for (Voucher voucher : voucherList) {
+
+            if (voucher.getVoucherCode().equals(voucherCode)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Đếm số lượng voucher
+    public int countVoucher() {
+        return voucherList.size();
+    }
+
+    // Hiển thị danh sách voucher
+    public void displayVoucher() {
+
+        for (Voucher voucher : voucherList) {
+
+            System.out.println("Voucher Code: "
+                    + voucher.getVoucherCode());
+
+            System.out.println("Discount Value: "
+                    + voucher.getDiscountValue());
+
+            System.out.println("--------------------");
+        }
+    }
+
+    // Getter
+    public ArrayList<Voucher> getVoucherList() {
+        return voucherList;
+    }
+
+    // Setter
+    public void setVoucherList(ArrayList<Voucher> voucherList) {
+        this.voucherList = voucherList;
+    }
 }
