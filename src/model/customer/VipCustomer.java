4176ee9460153ffa3319c7df9a8feb251f9f34ce
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-06-15 19:04:55 +0700

    Update VipCustomer.java

--- src/model/customer/VipCustomer.java
+++ src/model/customer/VipCustomer.java
@@ -1,13 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.customer;
 
-/**
- *
- * @author Nguyen Hoang Duy
- */
-public class VipCustomer {
-    
+public class VipCustomer extends Customer {
+
+    // Thuộc tính riêng của khách hàng VIP
+    private double vipDiscount;
+
+    // Constructor
+    public VipCustomer(String customerID,
+                       String customerName,
+                       String phoneNumber,
+                       String address,
+                       double vipDiscount) {
+
+        super(customerID, customerName, phoneNumber, address);
+        this.vipDiscount = vipDiscount;
+    }
+
+    // Getter
+    public double getVipDiscount() {
+        return vipDiscount;
+    }
+
+    // Setter
+    public void setVipDiscount(double vipDiscount) {
+        this.vipDiscount = vipDiscount;
+    }
+
+    // Tính số tiền giảm giá
+    public double calculateDiscount() {
+        return getTotalPurchase() * vipDiscount;
+    }
 }
