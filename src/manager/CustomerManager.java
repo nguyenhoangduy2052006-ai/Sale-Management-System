1a11010b6521a01946f55817c5e7799338154f7b
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-06-16 20:09:03 +0700

    Update CustomerManager.java

--- src/manager/CustomerManager.java
+++ src/manager/CustomerManager.java
@@ -3,91 +3,67 @@ package manager;
 import model.customer.Customer;
 import java.util.ArrayList;
 
-public class CustomerManagement {
+public class CustomerManager {
 
-    // Encapsulation
     private ArrayList<Customer> customerList;
 
-    // Constructor
-    public CustomerManagement() {
+    public CustomerManager() {
         customerList = new ArrayList<>();
     }
 
-    // Thêm khách hàng
-    public void addNewCustomer(Customer customer) {
-        customerList.add(customer);
-    }
-
-    // Cập nhật thông tin khách hàng
-    public void updateCustomerInfor(String customerID,
-                                    String name,
-                                    String phone,
-                                    String address) {
-
-        for (Customer customer : customerList) {
-
-            if (customer.getCustomerID().equals(customerID)) {
-
-                customer.updateCustomerInfor(
-                        name,
-                        phone,
-                        address
-                );
-                break;
-            }
+    // Add
+    public void addCustomer(Customer customer) {
+        try {
+            customerList.add(customer);
+        } catch (Exception e) {
+            System.out.println("Add customer failed!");
         }
     }
 
-    // Xóa khách hàng theo ID
-    public void removeCustomer(String customerID) {
-
-        for (int i = 0; i < customerList.size(); i++) {
-
-            if (customerList.get(i).getCustomerID().equals(customerID)) {
-
-                customerList.remove(i);
-                break;
+    // Search
+    public Customer searchCustomer(String id) {
+        for (Customer c : customerList) {
+            if (c.getCustomerID().equalsIgnoreCase(id)) {
+                return c;
             }
         }
-    }
-
-    // Tìm khách hàng theo ID
-    public Customer searchCustomerByID(String customerID) {
-
-        for (Customer customer : customerList) {
-
-            if (customer.getCustomerID().equals(customerID)) {
-                return customer;
-            }
-        }
-
         return null;
     }
 
-    // Hiển thị danh sách khách hàng
-    public void listCustomer() {
+    // Update
+    public boolean updateCustomer(String id, String name,
+                                  String phone, String address) {
+        Customer c = searchCustomer(id);
 
-        for (Customer customer : customerList) {
-
-            System.out.println("ID: " + customer.getCustomerID());
-            System.out.println("Name: " + customer.getCustomerName());
-            System.out.println("Phone: " + customer.getPhoneNumber());
-            System.out.println("Address: " + customer.getAddress());
-            System.out.println("Total Purchase: " + customer.getTotalPurchase());
-
-            System.out.println("--------------------");
+        if (c != null) {
+            c.setCustomerName(name);
+            c.setPhoneNumber(phone);
+            c.setAddress(address);
+            return true;
         }
+
+        return false;
     }
 
-    // Getter
-    public ArrayList<Customer> getCustomerList() {
-        return customerList;
+    // Remove
+    public boolean removeCustomer(String id) {
+        Customer c = searchCustomer(id);
+
+        if (c != null) {
+            try {
+                return customerList.remove(c);
+            } catch (Exception e) {
+                System.out.println("Remove customer failed!");
+            }
+        }
+
+        return false;
     }
 
-    // Setter
-    public void setCustomerList(ArrayList<Customer> customerList) {
-        this.customerList = customerList;
-    }
-}
+    // Display
+    public void displayCustomers() {
+        for (Customer c : customerList) {
+            System.out.println(c);
+        }
     }
 }
