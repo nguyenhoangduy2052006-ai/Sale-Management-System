7854dd89f3bc9a4b9d84ff6a4db3f54afc049eba
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-06-16 20:12:59 +0700

    Update VipCustomer.java

--- src/model/customer/VipCustomer.java
+++ src/model/customer/VipCustomer.java
@@ -2,32 +2,22 @@ package model.customer;
 
 public class VipCustomer extends Customer {
 
-    // Thuộc tính riêng của khách hàng VIP
     private double vipDiscount;
 
-    // Constructor
-    public VipCustomer(String customerID,
-                       String customerName,
-                       String phoneNumber,
-                       String address,
+    public VipCustomer(String customerID, String customerName,
+                       String phoneNumber, String address,
+                       double totalPurchase,
                        double vipDiscount) {
-
-        super(customerID, customerName, phoneNumber, address);
+        super(customerID, customerName, phoneNumber,
+              address, totalPurchase);
         this.vipDiscount = vipDiscount;
     }
 
-    // Getter
     public double getVipDiscount() {
         return vipDiscount;
     }
 
-    // Setter
     public void setVipDiscount(double vipDiscount) {
         this.vipDiscount = vipDiscount;
-    }
-
-    // Tính số tiền giảm giá
-    public double calculateDiscount() {
-        return getTotalPurchase() * vipDiscount;
     }
 }
