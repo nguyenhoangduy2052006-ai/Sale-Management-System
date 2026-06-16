220a8c847a89c3645c6d757c1e1e81756df3bdaa
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-06-16 20:12:29 +0700

    Update RegularCustomer.java

--- src/model/customer/RegularCustomer.java
+++ src/model/customer/RegularCustomer.java
@@ -2,31 +2,22 @@ package model.customer;
 
 public class RegularCustomer extends Customer {
 
-    // Thuộc tính riêng của RegularCustomer
     private double regularDiscount;
 
-    // Constructor
-    public RegularCustomer(String customerID,
-                           String customerName,
-                           String phoneNumber,
-                           String address,
+    public RegularCustomer(String customerID, String customerName,
+                           String phoneNumber, String address,
+                           double totalPurchase,
                            double regularDiscount) {
-
-        super(customerID, customerName, phoneNumber, address);
+        super(customerID, customerName, phoneNumber,
+              address, totalPurchase);
         this.regularDiscount = regularDiscount;
     }
 
-    // Getter
     public double getRegularDiscount() {
         return regularDiscount;
     }
 
-    // Setter
     public void setRegularDiscount(double regularDiscount) {
         this.regularDiscount = regularDiscount;
     }
-
-    // Tính giảm giá
-    public double calculateDiscount() {
-        return getTotalPurchase() * regularDiscount;
-    }
+}
