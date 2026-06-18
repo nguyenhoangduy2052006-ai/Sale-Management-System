36f4a2074200490a4c8c22b89610b1f22f27e4d1
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-06-18 07:22:05 +0700

    Update VoucherMenu.java

--- src/ui/VoucherMenu.java
+++ src/ui/VoucherMenu.java
@@ -1,8 +1,8 @@
-
 package ui;
 
 import java.util.Scanner;
 import manager.VoucherManager;
+import model.voucher.Voucher;
 
 public class VoucherMenu {
 
@@ -27,24 +27,74 @@ public class VoucherMenu {
             System.out.println("0. Exit");
             System.out.print("Choose: ");
 
-            choice = sc.nextInt();
+            choice = Integer.parseInt(sc.nextLine());
 
             switch (choice) {
 
                 case 1:
-                    System.out.println("Add Voucher");
+                    System.out.print("Enter Voucher ID: ");
+                    String id = sc.nextLine();
+
+                    System.out.print("Enter Voucher Code: ");
+                    String code = sc.nextLine();
+
+                    System.out.print("Enter Discount Value: ");
+                    double discount = Double.parseDouble(sc.nextLine());
+
+                    System.out.print("Enter Expiry Date: ");
+                    String expiryDate = sc.nextLine();
+
+                    System.out.print("Enter Status (true/false): ");
+                    boolean status = Boolean.parseBoolean(sc.nextLine());
+
+                    Voucher voucher = new Voucher(
+                            id,
+                            code,
+                            discount,
+                            expiryDate,
+                            status
+                    );
+
+                    voucherManager.addVoucher(voucher);
+                    System.out.println("Add Voucher Successfully!");
                     break;
 
                 case 2:
-                    System.out.println("Update Voucher");
+                    System.out.print("Enter Voucher Code: ");
+                    code = sc.nextLine();
+
+                    System.out.print("Enter New Discount Value: ");
+                    discount = Double.parseDouble(sc.nextLine());
+
+                    if (voucherManager.updateVoucher(code, discount)) {
+                        System.out.println("Update Successfully!");
+                    } else {
+                        System.out.println("Voucher Not Found!");
+                    }
                     break;
 
                 case 3:
-                    System.out.println("Remove Voucher");
+                    System.out.print("Enter Voucher Code: ");
+                    code = sc.nextLine();
+
+                    if (voucherManager.removeVoucher(code)) {
+                        System.out.println("Remove Successfully!");
+                    } else {
+                        System.out.println("Voucher Not Found!");
+                    }
                     break;
 
                 case 4:
-                    System.out.println("Search Voucher");
+                    System.out.print("Enter Voucher Code: ");
+                    code = sc.nextLine();
+
+                    Voucher v = voucherManager.searchVoucher(code);
+
+                    if (v != null) {
+                        System.out.println(v);
+                    } else {
+                        System.out.println("Voucher Not Found!");
+                    }
                     break;
 
                 case 5:
@@ -56,10 +106,11 @@ public class VoucherMenu {
                     break;
 
                 default:
-                    System.out.println("Invalid choice!");
+                    System.out.println("Invalid Choice!");
             }
 
         } while (choice != 0);
     }
 }
+
 
