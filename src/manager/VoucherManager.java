45e5a7f00243aa3775a1f4990cc5d9d5cc66d164
Ngocthao2705 <265555204+lengocthaodmx-create@users.noreply.github.com>
2026-07-04 15:58:30 +0700

    Update VoucherManager.java

--- src/manager/VoucherManager.java
+++ src/manager/VoucherManager.java
@@ -4,9 +4,6 @@ import model.voucher.Voucher;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-// 👉 CHỈ THÊM IMPORT NÀY
-import java.io.*;
-
 public class VoucherManager {
 
     private ArrayList<Voucher> voucherList;
@@ -112,67 +109,4 @@ public class VoucherManager {
         return v.isStatus()
                 && !v.getExpiryDate().isBefore(today);
     }
-
-    // =========================
-    // ✅ FILE I/O - SAVE (ONLY ADD)
-    // =========================
-    public void saveToFile(String filename) {
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter(filename));
-
-            for (Voucher v : voucherList) {
-                bw.write(v.getVoucherID() + ","
-                        + v.getVoucherCode() + ","
-                        + v.getDiscountValue() + ","
-                        + v.getExpiryDate() + ","
-                        + v.isStatus());
-                bw.newLine();
-            }
-
-            bw.close();
-            System.out.println("Save file success!");
-
-        } catch (IOException e) {
-            System.out.println("Save file error!");
-        }
-    }
-
-    // =========================
-    // ✅ FILE I/O - LOAD (ONLY ADD)
-    // =========================
-    public void loadFromFile(String filename) {
-        try {
-            File file = new File(filename);
-
-            if (!file.exists()) {
-                System.out.println("File not found!");
-                return;
-            }
-
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line;
-
-            while ((line = br.readLine()) != null) {
-
-                String[] p = line.split(",");
-
-                Voucher v = new Voucher(
-                        p[0],
-                        p[1],
-                        Double.parseDouble(p[2]),
-                        LocalDate.parse(p[3]),
-                        Boolean.parseBoolean(p[4])
-                );
-
-                voucherList.add(v);
-            }
-
-            br.close();
-            System.out.println("Load file success!");
-
-        } catch (Exception e) {
-            System.out.println("Load file error!");
-        }
-    }
-
 }
